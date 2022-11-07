8c10259be9900ec412127531d2d55c1e01876ea2
Kennedy Mahoro <106330299+kennedymahoro@users.noreply.github.com>
2022-11-07 10:53:40 -0500

    added all of the buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,29 @@
 function App(): JSX.Element {
   return (
-    <div className="flex bg-white justify-center  ">Hello Wold</div>
-  )
+    <div>
+      <div className="grid">
+        <div>125</div>
+        <button>clear</button>
+        <button>del</button>
+        <button>+</button>
+        <button>1</button>
+        <button>2</button>
+        <button>3</button>
+        <button>-</button>
+        <button>4</button>
+        <button>5</button>
+        <button>6</button>
+        <button>*</button>
+        <button>7</button>
+        <button>8</button>
+        <button>9</button>
+        <button>/</button>
+        <button>.</button>
+        <button>0</button>
+        <button>=</button>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
