04cf2020cebe2e958eae40c3bb4a7577baf92630
Kennedy Mahoro <106330299+kennedymahoro@users.noreply.github.com>
2022-11-09 21:17:38 -0500

    limit the width size

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,28 @@
 function App(): JSX.Element {
   return (
     <div>
-      <div className="grid grid-cols-4">
-        <div className="col-span-full w-32 ">125</div>
-        <button className="col-span-2">clear</button>
-        <button>del</button>
-        <button>+</button>
-        <button>1</button>
-        <button>2</button>
-        <button>3</button>
-        <button>-</button>
-        <button>4</button>
-        <button>5</button>
-        <button>6</button>
-        <button>*</button>
-        <button>7</button>
-        <button>8</button>
-        <button>9</button>
-        <button>/</button>
-        <button>.</button>
-        <button>0</button>
-        <button className="col-span-2">=</button>
+      <div className="w-16">
+        <div className="grid grid-cols-4">
+          <div className="col-span-full w-32 ">125</div>
+          <button className="col-span-2">clear</button>
+          <button>del</button>
+          <button>+</button>
+          <button>1</button>
+          <button>2</button>
+          <button>3</button>
+          <button>-</button>
+          <button>4</button>
+          <button>5</button>
+          <button>6</button>
+          <button>*</button>
+          <button>7</button>
+          <button>8</button>
+          <button>9</button>
+          <button>/</button>
+          <button>.</button>
+          <button>0</button>
+          <button className="col-span-2">=</button>
+        </div>
       </div>
     </div>
   );
