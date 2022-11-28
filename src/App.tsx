8070b9aba57ef5856bcbd47d7d6e3dcee7bd9851
Kennedy Mahoro <106330299+kennedymahoro@users.noreply.github.com>
2022-11-28 10:35:50 -0500

    fixed the positioning issue, and changed button color

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,26 @@
 function App(): JSX.Element {
   return (
-    <div className="flex justify-center items-center">
-      <div className="w-20">
-        <div className="grid grid-cols-4">
-          <div className="col-span-full w-32 ">125</div>
-          <button className="col-span-2">clear</button>
-          <button>del</button>
-          <button>+</button>
-          <button>1</button>
-          <button>2</button>
-          <button>3</button>
-          <button>-</button>
-          <button>4</button>
-          <button>5</button>
-          <button>6</button>
-          <button>*</button>
-          <button>7</button>
-          <button>8</button>
-          <button>9</button>
-          <button>/</button>
-          <button>.</button>
-          <button>0</button>
-          <button className="col-span-2">=</button>
-        </div>
+    <div className="flex justify-center items-center h-screen">
+      <div className="grid grid-cols-4">
+        <div className="col-span-full w-32 bg-blue-200">125</div>
+        <button className="col-span-2 bg-red-50">clear</button>
+        <button className="bg-orange-500">del</button>
+        <button className="bg-blue-50">+</button>
+        <button className="bg-yellow-200">1</button>
+        <button className="bg-yellow-200">2</button>
+        <button className="bg-yellow-200">3</button>
+        <button className="bg-blue-50">-</button>
+        <button className="bg-yellow-200">4</button>
+        <button className="bg-yellow-200">5</button>
+        <button className="bg-yellow-200">6</button>
+        <button className="bg-blue-50">*</button>
+        <button className="bg-yellow-200">7</button>
+        <button className="bg-yellow-200">8</button>
+        <button className="bg-yellow-200">9</button>
+        <button className="bg-blue-50">/</button>
+        <button className="bg-yellow-200">.</button>
+        <button className="bg-yellow-200">0</button>
+        <button className="col-span-2">=</button>
       </div>
     </div>
   );
